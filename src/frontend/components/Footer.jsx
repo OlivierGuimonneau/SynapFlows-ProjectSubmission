2a80badcfe95916554f10a52a727c3f55a684f3b
Olivier Guimonneau <olivier.guimonneau@outlook.fr>
2026-04-08 11:35:19 +0200

    Modif footer Mentions légales

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="footer">
-      © 2026 SynapFlows
+      © 2026 SynapFlows - Mentions légales
     </footer>
   );
 }
